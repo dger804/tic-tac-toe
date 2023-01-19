4fba88f3f9ad15c5b29f7230ad9bf152595c7da0
Diego Gutiérrez <diego804@gmail.com>
2023-01-18 21:30:11 -0500

    install standard eslinter and correction with eslinter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react"
+import { useState } from 'react'
 import confetti from 'canvas-confetti'
-import { Square } from "./components/Square"
+import { Square } from './components/Square'
 import { TURNS } from './constants'
-import { checkWinner, checkEndGame } from "./logic/board"
-import { WinnerModal } from "./components/WinnerModal"
-import { resetGameStorage, saveGame } from "./logic/storage"
+import { checkWinner, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+import { resetGameStorage, saveGame } from './logic/storage'
 
-function App() {
+function App () {
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board')
 
@@ -20,9 +20,9 @@ function App() {
   const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
-    if (board[index] || winner) return //si ya tiene algo no permite sobreescribir || si hay ganador tampoco deja escribir
+    if (board[index] || winner) return // si ya tiene algo no permite sobreescribir || si hay ganador tampoco deja escribir
 
-    const newBoard = [...board] //actualizar tablero
+    const newBoard = [...board] // actualizar tablero
     newBoard[index] = turn
     setBoard(newBoard)
 
@@ -48,10 +48,10 @@ function App() {
   }
 
   return (
-    <main className="board">
+    <main className='board'>
       <h1> Tic Tac Toe </h1>
       <button onClick={resetGame}>Reset del Juego</button>
-      <section className="game">
+      <section className='game'>
         {
           board.map((_, index) => {
             return (
@@ -66,7 +66,7 @@ function App() {
           })
         }
       </section>
-      <section className="turn">
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
--- src/logic/storage/index.js
+++ src/logic/storage/index.js
@@ -1,6 +1,5 @@
 
 export const saveGame = ({ board, turn }) => {
-
   window.localStorage.setItem('board', JSON.stringify(board))
   window.localStorage.setItem('turn', turn)
 }
